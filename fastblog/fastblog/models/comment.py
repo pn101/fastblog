9a0a0c53326d67b7a81f80c835aeef69acc6fdb3
pn101 <philipnam101@gmail.com>
2016-06-15 22:19:25 +0900

    init Comment model

---
+++ fastblog/fastblog/models/comment.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Comment(models.Model):
+
+    post = models.ForeignKey('Post')
+
+    user = models.ForeignKey(User)
+
+    content = models.TextField()
+
+    def __str__(self):
+        return self.content
+
+    def get_absolute_url(self):
+        return reverse(
+                'fastblog:detail',
+                kwargs={
+                    'pk': self.post.id,
+                }
+        ) + '#comment-' + str(self.id)
