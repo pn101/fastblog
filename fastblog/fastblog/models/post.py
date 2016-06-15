f0ca304e52a04e69dec652c021b0bf53abc9c026
pn101 <philipnam101@gmail.com>
2016-06-15 20:57:29 +0900

    init Post model: makemigrate, and migrate

---
+++ fastblog/fastblog/models/post.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+
+
+class PostManager(models.Manager):
+
+    def public(self):
+        return self.filter(is_public=True)
+
+
+class Post(models.Model):
+
+    objects = PostManager()
+
+    user = models.ForeignKey(User)
+
+    title = models.CharField(
+            max_length=120,
+    )
+
+    content = models.TextField()
+
+    image = models.ImageField()
+
+    is_public = models.BooleanField(
+            default=True,
+    )
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        pass
