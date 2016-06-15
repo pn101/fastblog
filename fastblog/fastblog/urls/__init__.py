47d4d00a4abc105042440725be7063a7797a4870
pn101 <philipnam101@gmail.com>
2016-06-15 20:13:45 +0900

    init HomeView: and all corresponding templates and urls

--- fastblog/fastblog/urls/__init__.py
+++ fastblog/fastblog/urls/__init__.py
@@ -1,9 +1,14 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 from fastblog.views import *
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+
+    url(r'^$', HomeView.as_view(), name='home'),
+
+] + static(settings.MEDIA_URL, documents_root=settings.MEDIA_ROOT)
