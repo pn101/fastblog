37a55639874ab093c1027324341cdac267c043e9
pn101 <philipnam101@gmail.com>
2016-06-15 20:42:49 +0900

    init LogoutView: and all corresponding templates and urls

--- fastblog/fastblog/urls/auth.py
+++ fastblog/fastblog/urls/auth.py
@@ -6,5 +6,6 @@ from fastblog.views.auth import *
 urlpatterns = [
     url(r'^signup/$', SignupView.as_view(), name='signup'),
     url(r'^login/$', LoginView.as_view(), name='login'),
+    url(r'^logout/$', LogoutView.as_view(), name='logout'),
 
 ]
