cb14dcf53b1490899c7db7988d73422acefe4420
pn101 <philipnam101@gmail.com>
2016-06-15 20:51:51 +0900

    added LoginRequiredMixin: to LogoutView and MyPageView

--- fastblog/fastblog/views/auth/logout.py
+++ fastblog/fastblog/views/auth/logout.py
@@ -1,9 +1,10 @@
 from django.views.generic import View
 from django.contrib.auth import logout
 from django.shortcuts import redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class LogoutView(View):
+class LogoutView(LoginRequiredMixin, View):
 
     def get(self, request, *args, **kwargs):
         logout(request)
--- fastblog/fastblog/views/auth/mypage.py
+++ fastblog/fastblog/views/auth/mypage.py
@@ -1,7 +1,8 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class MyPageView(TemplateView):
+class MyPageView(LoginRequiredMixin, TemplateView):
     template_name = 'auth/mypage.html'
 
     def get_context_data(self, **kwargs):
