b11b93a83c27a8d00d67248f147afb493e97105a
pn101 <philipnam101@gmail.com>
2016-06-17 01:30:19 +0900

    further Bitly Signals refactoring

--- fastblog/bitly/models/bitly.py
+++ fastblog/bitly/models/bitly.py
@@ -2,10 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
-from django.dispatch import receiver
-from django.db.models.signals import post_save
-from hashids import Hashids
-
 
 class BitLink(models.Model):
 
@@ -30,11 +26,3 @@ class BitLink(models.Model):
                     'shorten_hash': self.shorten_hash,
                 }
         )
-
-
-@receiver(post_save, sender=BitLink)
-def post_save_bitlink(sender, instance, created, **kwargs):
-    if created:
-        hashids = Hashids(salt="awesome bitlink", min_length=4)
-        instance.shorten_hash = hashids.encode(instance.id)
-        instance.save()
