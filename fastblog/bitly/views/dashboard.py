69b208a909b9f8b7213691177f3c3cb8da46cd79
pn101 <philipnam101@gmail.com>
2016-06-16 17:43:04 +0900

    update BitLinkLog on MyPage

--- fastblog/bitly/views/dashboard.py
+++ fastblog/bitly/views/dashboard.py
@@ -6,3 +6,8 @@ from bitly.models import BitLink
 class BitLinkDashboardView(ListView):
     model = BitLink
     template_name = 'dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(BitLinkDashboardView, self).get_context_data(**kwargs)
+        context['site_name'] = 'Bitly Dashboard'
+        return context
