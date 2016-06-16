056bc9600e906a7a0dc39501258d54a0ddbc2892
pn101 <philipnam101@gmail.com>
2016-06-16 21:19:24 +0900

    init BitLinkRedirectView: and all corresponding templates and urls

---
+++ fastblog/bitly/views/redirect.py
@@ -0,0 +1,12 @@
+from django.views.generic.base import RedirectView
+from django.shortcuts import get_object_or_404
+
+
+class BitLinkRedirectView(RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        bitlink = get_object_or_404(
+                self.request.user.bitlink_set,
+                shorten_hash=kwargs.get('shorten_hash'),
+        )
+        return bitlink.original_url
