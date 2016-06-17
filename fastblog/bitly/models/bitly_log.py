1f9cd0234e641e11008a65443f9616cc5312f178
pn101 <philipnam101@gmail.com>
2016-06-17 13:25:10 +0900

    update BitLinkLog model

--- fastblog/bitly/models/bitly_log.py
+++ fastblog/bitly/models/bitly_log.py
@@ -6,3 +6,27 @@ class BitLinkLog(models.Model):
     bitlink = models.ForeignKey('BitLink')
 
     created_at = models.DateTimeField(auto_add_now=True)
+
+    http_referr = models.CharField(
+            max_length=255,
+            blank=True,
+            null=True,
+    )
+
+    http_user_agent = models.CharField(
+            max_length=255,
+            blank=True,
+            null=True,
+    )
+
+    logname = models.CharfField(
+            max_length=120,
+            blank=True,
+            null=True,
+    )
+
+    request_addr = models.CharField(
+            max_length=31,
+            blank=True,
+            null=True,
+    )
