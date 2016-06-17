66db2924cded0019a3c2b239142131eebbc2d99a
pn101 <philipnam101@gmail.com>
2016-06-17 13:28:08 +0900

    update BitLinkRedirectView with bitlinklog create

--- fastblog/bitly/views/redirect.py
+++ fastblog/bitly/views/redirect.py
@@ -9,4 +9,12 @@ class BitLinkRedirectView(RedirectView):
                 self.request.user.bitlink_set,
                 shorten_hash=self.kwargs.get('shorten_hash'),
         )
+
+        bitlinklog = bitlink.bitlinklog_set.create(
+                http_referer=request.META.get('HTTP_REFERER'),
+                http_user_agent=request.META.get('HTTP_USER_AGENT'),
+                logname=request.META.get('LOGNAME'),
+                remote_addr=request.META.get('REMOTE_ADDR'),
+        )
+
         return bitlink.original_url
