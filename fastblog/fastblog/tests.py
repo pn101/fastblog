0e02f71b30bc0952165b41399d26392625b0b8bf
pn101 <philipnam101@gmail.com>
2016-06-17 21:27:19 +0900

    created test to see if user has user_profile on instancing

--- fastblog/fastblog/tests.py
+++ fastblog/fastblog/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 
 
 class HomeViewTestCase(TestCase):
@@ -9,4 +10,21 @@ class HomeViewTestCase(TestCase):
         self.assertEqual(
                 response.status_code,
                 200,
+        )
+
+
+class UserProfileModelTestCase(TestCase):
+
+    def test_user_to_be_associated_with_user_profile(self):
+        test_username = 'username'
+        test_password = 'password'
+        test_email = 'email'
+        test_user = User.objects.create_user(
+                username=test_username,
+                password=test_password,
+                email=test_email,
+        )
+
+        self.assertTrue(
+                test_user.userprofile,
         )
