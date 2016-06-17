f834d084ba0c09fbca2152b7f689fb962b0e8847
pn101 <philipnam101@gmail.com>
2016-06-17 16:59:49 +0900

    update test.py

--- fastblog/fastblog/tests.py
+++ fastblog/fastblog/tests.py
@@ -3,9 +3,11 @@ from django.test import TestCase
 
 class PricingViewTestCase(TestCase):
 
+    def setUp(self):
+        self.response = self.client.get('/pricing/')
+
     def test_pricing_should_return_200(self):
-        response = self.client.get('/pricing/')
         self.assertEqual(
-                response.status_code,
+                self.response.status_code,
                 200,
         )
